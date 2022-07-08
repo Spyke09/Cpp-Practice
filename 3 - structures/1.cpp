b6a38718c20d5159489a2a7f934f7e87067ef06a
Spyke09 <romanov.alexanderromanov@yandex.ru>
2022-07-08 15:10:32 +0300

    task 3 and 5

---
+++ 3 - structures/1.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+struct Ratio {
+private:
+    int num, denom;
+public:
+    double to_double() const {
+        return static_cast<double>(num)/denom;
+    }
+    
+    Ratio(int num, int denom): num(num), denom(denom) {};
+
+    int getNum()  const{
+        return num;
+    }
+
+    int getDenom()  const{
+        return denom;
+    }
+
+    Ratio operator/(const Ratio &other) const {
+        return {num * other.denom, denom * other.num};
+    }
+
+    friend Ratio operator+(const Ratio &a, const Ratio &b);
+    friend std::ostream& operator<<(std::ostream &os, const Ratio &a);
+
+    Ratio operator++() {
+        num += denom;
+        return *this;
+    }
+    Ratio operator++(int){
+        Ratio old = *this;
+        ++*this;
+        return old;
+    }
+};
+
+Ratio operator/(const Ratio &a, const Ratio &b) {
+    return {a.getNum() * b.getDenom(), a.getDenom() * b.getNum()};
+}
+
+Ratio operator+(const Ratio &a, const Ratio &b) {
+    return {a.num*b.denom + b.num*a.denom, a.denom*b.denom};
+}
+
+std::ostream& operator<<(std::ostream &os, const Ratio &a) {
+    return os<<a.num<<'/'<<a.denom;
+}
+
+int main() {
+    [[maybe_unused]]Ratio a1(10, 20);
+    [[maybe_unused]]Ratio a2 = {1, 3};
+    [[maybe_unused]]Ratio a3{1, 3};
+    [[maybe_unused]]Ratio a4(1, 3);
+
+    std::cout<<operator/(a1, a2)<<"\n";
+    std::cout<<a1.operator/(a2)<<"\n";
+    std::cout<<a1+a2<<"\n";
+    std::cout<<++a1<<"\n";
+    std::cout<<a1++<<"\n";
+    std::cout<<a1<<"\n";
+    return 0;
+}
---
+++ 5 - conversion/01-constructor.cpp
@@ -0,0 +1,23 @@
+#include <cassert>
+#include <iostream>
+
+struct BigInt {
+    BigInt(int x) {
+        static_cast<void>(x);
+        [[maybe_unused]]int a;
+    }
+};
+
+void foo(BigInt) {
+    
+}
+
+BigInt bar() {
+    return 40;
+}
+
+int main() {
+    BigInt a = 10;
+    a = 12;
+    foo(23);
+}
